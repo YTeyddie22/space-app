5b9b3dc7584f6703b631aa5ecea770bc884aaad1
Teddy Yongo <teyddie@gmail.com>
2022-06-29 12:40:19 +0300

    fetching planets

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,26 +1,29 @@
+//API URL
+const API_URL = `http://localhost:8000`;
+
 async function httpGetPlanets() {
-  // TODO: Once API is ready.
-  // Load planets and return as JSON.
+	// TODO: Once API is ready.
+
+	const response = await fetch(`${API_URL}/planets`);
+
+	// Load planets and return as JSON.
+
+	return await response.json();
 }
 
 async function httpGetLaunches() {
-  // TODO: Once API is ready.
-  // Load launches, sort by flight number, and return as JSON.
+	// TODO: Once API is ready.
+	// Load launches, sort by flight number, and return as JSON.
 }
 
 async function httpSubmitLaunch(launch) {
-  // TODO: Once API is ready.
-  // Submit given launch data to launch system.
+	// TODO: Once API is ready.
+	// Submit given launch data to launch system.
 }
 
 async function httpAbortLaunch(id) {
-  // TODO: Once API is ready.
-  // Delete launch with given ID.
+	// TODO: Once API is ready.
+	// Delete launch with given ID.
 }
 
-export {
-  httpGetPlanets,
-  httpGetLaunches,
-  httpSubmitLaunch,
-  httpAbortLaunch,
-};
+export {httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch};
