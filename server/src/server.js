b34c5d049396767fb05d7fa43d2afb3f198ab7f9
Teddy Yongo <teyddie@gmail.com>
2022-06-29 11:35:14 +0300

    changes

--- server/src/server.js
+++ server/src/server.js
@@ -6,5 +6,5 @@ const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
-	console.log(`Listening to port ${PORT}`);
+	console.log(`Listenin to port ${PORT}`);
 });
