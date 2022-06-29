3fab1b3213a807f85e79e9ab06fe50326fb1f691
Teddy Yongo <teyddie@gmail.com>
2022-06-29 13:04:36 +0300

    adding cors for access

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,19 @@
 const express = require('express');
 
+const cors = require('cors');
+
+const planetRouter = require('./routes/planetRoute');
+
 const app = express();
 
+app.use(
+	cors({
+		origin: `http://localhost:3000`,
+	})
+);
+
 app.use(express.json());
+
+app.use(planetRouter);
 
 module.exports = app;
