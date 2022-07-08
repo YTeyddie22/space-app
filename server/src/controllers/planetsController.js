62a8068ff1b76b82edd5ffe3eb330cf3d510333a
Teddy Yongo <teyddie@gmail.com>
2022-07-08 13:42:01 +0300

    parsing data to the model

--- server/src/controllers/planetsController.js
+++ server/src/controllers/planetsController.js
@@ -1,7 +1,10 @@
 const planets = require('../models/Planets');
 
 const getAllPlanets = (req, res) => {
-	return res.status(200).json(planets);
+	return res.status(200).json({
+		status: 'success',
+		planets,
+	});
 };
 
 module.exports = {
--- server/src/models/Planets.js
+++ server/src/models/Planets.js
@@ -1,3 +1,39 @@
-const planets = [];
+const {parse} = require('csv-parse');
+const fs = require('fs');
 
-module.exports = planets;
+const habitablePlanet = [];
+const stream = parse({
+	comment: '#',
+	columns: true,
+});
+
+function isHabitable(planet) {
+	return (
+		planet['koi_disposition'] === 'CONFIRMED' &&
+		planet['koi_insol'] > 0.36 &&
+		planet['koi_insol'] < 1.11 &&
+		planet['koi_prad'] < 1.6
+	);
+}
+
+fs.createReadStream('data/kepler_data.csv')
+	.pipe(stream)
+	.on('data', (data) => {
+		if (isHabitable(data)) {
+			habitablePlanet.push(data);
+		}
+	})
+	.on('end', () => {
+		console.log(
+			habitablePlanet.map((planet) => {
+				return planet['kepler_name'];
+			})
+		);
+	})
+	.on('error', (err) => {
+		console.log(err);
+	});
+
+module.exports = {
+	planets: habitablePlanet,
+};
