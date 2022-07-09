3dedb1a943fcce2635163ccb54368e6050c8419c
Teddy Yongo <teyddie@gmail.com>
2022-07-09 08:23:07 +0300

    loading data on startup

--- server/src/server.js
+++ server/src/server.js
@@ -2,9 +2,16 @@ const http = require('http');
 const app = require('./app');
 
 const PORT = process.env.PORT || 8000;
+const {loadPlanetsData} = require('./models/Planets');
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-	console.log(`Listenin to port ${PORT}`);
-});
+async function startServer() {
+	await loadPlanetsData();
+
+	server.listen(PORT, () => {
+		console.log(`Listenin to port ${PORT}`);
+	});
+}
+
+startServer();
