6292aa13f2c17e0021e31b2af392e0cbd4ed11e8
Teddy Yongo <teddy@aeeiee.com>
2023-10-07 13:14:45 +0300

    adjusting spaces and adding pm2

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -4,9 +4,8 @@ const API_URL = `http://localhost:8000`;
 //! Load planets and return as JSON.
 async function httpGetPlanets() {
 	const response = await fetch(`${API_URL}/planets`);
-
-	console.log(response);
-	return await response.json();
+	const result = await response.json();
+	return result;
 }
 
 //! Load launches, sort by flight number, and return as JSON.
@@ -14,8 +13,8 @@ async function httpGetLaunches() {
 	const response = await fetch(`${API_URL}/launches`);
 
 	//* Get the launches and sort in ascending order;
-
 	const fetchedLaunches = await response.json();
+	console.log(fetchedLaunches);
 	return fetchedLaunches.sort((a, b) => a.flightNumber - b.flightNumber);
 }
 
@@ -29,4 +28,4 @@ async function httpAbortLaunch(id) {
 	// Delete launch with given ID.
 }
 
-export {httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch};
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
