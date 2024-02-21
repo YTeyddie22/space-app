a47eabe118ba2014aa4f6d222dd3e3574c28710c
Teddy Yongo <teyddie@gmail.com>
2024-02-21 06:47:37 +0300

    mapping the launches data

--- server/src/models/Launches.js
+++ server/src/models/Launches.js
@@ -29,6 +29,26 @@ async function loadLaunchesData() {
 			],
 		},
 	});
+	const launchDocs = response.data.docs;
+
+	for (const launchDoc of launchDocs) {
+		const payloads = launchDoc["payloads"];
+		const customers = payloads.flatMap((payload) => {
+			return payload["customers"];
+		});
+
+		const launch = {
+			flightNumber: launchDoc["flight_number"],
+			mission: launchDoc["name"],
+			rocket: launchDoc["rocket"]["name"],
+			launchDate: launchDoc["date_local"],
+			upcoming: launchDoc["upcoming"],
+			success: launchDoc["success"],
+			customers,
+		};
+
+		console.log(`${launch.flightNumber} ${launch.mission}`);
+	}
 }
 
 async function getAllLaunches() {
